3561bfb346e445a245d2c2b799a2ed8ba3d78c3f
joshuaadamhenderson <47343175+joshuaadamhenderson@users.noreply.github.com>
2022-02-01 21:33:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ function App() {
   return (
     <div className="App">
       <MainWindow />
+	  <ItemWindow />
     </div>
   );
 }
 
 class MainWindow extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return(
-			<div id="mainWindow">
-				TEST TEST TEST
-			</div>
-		)
-	}
+  constructor(props) {
+	super(props);
+  }
+  render() {
+	return(
+	  <div className="main-window">
+		TEST TEST TEST
+	  </div>
+	)
+  }
 }
 
+class ItemWindow extends React.Component {
+  constructor(props) {
+	super(props);
+  }
+  render() {
+    return(
+	  <div className="item-window">
+	  </div>
+	)
+  }
+}
 
 
 
